2b5cdd405503dbf0d1a895771790f673a3f2844c
Erick Cesar <e.cesar88@gmail.com>
2024-05-02 01:30:46 -0300

    fix: table not showing data

--- src/renderer/src/components/Clients/Read.tsx
+++ src/renderer/src/components/Clients/Read.tsx
@@ -1,8 +1,8 @@
 import { Client } from '@prisma/client'
+import { createColumnHelper } from '@tanstack/react-table'
 import { useContext } from 'react'
 import { SelectedRowContext, SelectedRowContextType } from '../../context/SelectedRowContext'
 import { CustomTable } from '../Table'
-import { ColumnDef } from '@tanstack/react-table'
 
 export const Read = ({
   clients,
@@ -13,55 +13,55 @@ export const Read = ({
 }): React.ReactNode => {
   const { selectedRow } = useContext<SelectedRowContextType<Client>>(SelectedRowContext)
 
-  // const columnHelper = createColumnHelper<Client>()
+  const columnHelper = createColumnHelper<Client>()
 
-  // const columns = [
-  //   columnHelper.accessor('id', {
-  //     id: 'id',
-  //     cell: (info) => info.getValue(),
-  //     header: () => <>Id</>,
-  //     maxSize: 80,
-  //     minSize: 80
-  //   }),
-  //   columnHelper.accessor('name', {
-  //     id: 'name',
-  //     cell: (info) => info.getValue(),
-  //     header: () => <>Nome</>,
-  //     maxSize: 256,
-  //     minSize: 256
-  //   }),
-  //   columnHelper.accessor('phone', {
-  //     id: 'phone',
-  //     cell: (info) => info.getValue(),
-  //     header: () => <>Telefone</>,
-  //     maxSize: 256,
-  //     minSize: 256
-  //   })
-  // ]
-
-  const columns: ColumnDef<Client>[] = [
-    {
+  const columns = [
+    columnHelper.accessor('id', {
       id: 'id',
       cell: (info) => info.getValue(),
       header: () => <>Id</>,
       maxSize: 80,
       minSize: 80
-    },
-    {
+    }),
+    columnHelper.accessor('name', {
       id: 'name',
       cell: (info) => info.getValue(),
       header: () => <>Nome</>,
       maxSize: 256,
       minSize: 256
-    },
-    {
+    }),
+    columnHelper.accessor('phone', {
       id: 'phone',
       cell: (info) => info.getValue(),
       header: () => <>Telefone</>,
       maxSize: 256,
       minSize: 256
-    }
+    })
   ]
+
+  // const columns: ColumnDef<Client>[] = [
+  //   {
+  //     id: 'id',
+  //     cell: (info) => info.getValue(),
+  //     header: () => <>Id</>,
+  //     maxSize: 80,
+  //     minSize: 80
+  //   },
+  //   {
+  //     id: 'name',
+  //     cell: (info) => info.getValue(),
+  //     header: () => <>Nome</>,
+  //     maxSize: 256,
+  //     minSize: 256
+  //   },
+  //   {
+  //     id: 'phone',
+  //     cell: (info) => info.getValue(),
+  //     header: () => <>Telefone</>,
+  //     maxSize: 256,
+  //     minSize: 256
+  //   }
+  // ]
 
   return (
     <CustomTable
