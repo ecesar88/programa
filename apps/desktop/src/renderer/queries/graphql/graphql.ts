f2c7bad40f5499cd9d65ef59e18811ba840e2252
Erick Cesar <e.cesar88@gmail.com>
2024-07-19 12:24:15 -0300

    feat: update graphql schema

--- apps/desktop/src/renderer/queries/graphql/graphql.ts
+++ apps/desktop/src/renderer/queries/graphql/graphql.ts
@@ -15,28 +15,43 @@ export type Scalars = {
   Boolean: { input: boolean; output: boolean; }
   Int: { input: number; output: number; }
   Float: { input: number; output: number; }
+  DateTime: { input: any; output: any; }
 };
 
+export type BaseError = Error & {
+  __typename?: 'BaseError';
+  message?: Maybe<Scalars['String']['output']>;
+};
+
+/** Long necks, cool patterns, taller than you. */
 export type Client = {
   __typename?: 'Client';
   id?: Maybe<Scalars['Int']['output']>;
   name?: Maybe<Scalars['String']['output']>;
-  orders?: Maybe<Array<Maybe<Order>>>;
   phone?: Maybe<Scalars['String']['output']>;
+};
+
+export type Error = {
+  message?: Maybe<Scalars['String']['output']>;
+};
+
+export type LengthError = Error & {
+  __typename?: 'LengthError';
+  message?: Maybe<Scalars['String']['output']>;
+  minLength?: Maybe<Scalars['Int']['output']>;
 };
 
 export type Mutation = {
   __typename?: 'Mutation';
-  createClient: Client;
-  deleteClient: Client;
-  updateClient: Client;
+  createClient?: Maybe<Client>;
+  deleteClient?: Maybe<Client>;
+  updateClient?: Maybe<Client>;
 };
 
 
 export type MutationCreateClientArgs = {
-  email: Scalars['String']['input'];
   name: Scalars['String']['input'];
-  phone: Scalars['String']['input'];
+  phone?: InputMaybe<Scalars['String']['input']>;
 };
 
 
@@ -44,39 +59,54 @@ export type MutationDeleteClientArgs = {
   id: Scalars['Int']['input'];
 };
 
-export type Order = {
-  __typename?: 'Order';
-  address?: Maybe<Scalars['String']['output']>;
-  dateTime?: Maybe<Scalars['String']['output']>;
-  food?: Maybe<Scalars['String']['output']>;
-  id?: Maybe<Scalars['Int']['output']>;
+
+export type MutationUpdateClientArgs = {
+  data: UserUpdateInput;
+  id: Scalars['Int']['input'];
 };
 
 export type Query = {
   __typename?: 'Query';
-  queryAllClient: Array<Maybe<Client>>;
-  queryOneClient: Client;
+  getAllClients?: Maybe<Client>;
+  getClientById?: Maybe<QueryGetClientByIdResult>;
 };
 
 
-export type QueryQueryAllClientArgs = {
+export type QueryGetAllClientsArgs = {
   page?: InputMaybe<Scalars['Int']['input']>;
 };
 
 
-export type QueryQueryOneClientArgs = {
+export type QueryGetClientByIdArgs = {
   id: Scalars['Int']['input'];
+};
+
+export type QueryGetClientByIdResult = BaseError | QueryGetClientByIdSuccess | RecordNotFoundError;
+
+export type QueryGetClientByIdSuccess = {
+  __typename?: 'QueryGetClientByIdSuccess';
+  data: Client;
+};
+
+export type RecordNotFoundError = Error & {
+  __typename?: 'RecordNotFoundError';
+  message?: Maybe<Scalars['String']['output']>;
+};
+
+export type UserUpdateInput = {
+  name: Scalars['String']['input'];
+  phone?: InputMaybe<Scalars['String']['input']>;
 };
 
 export type GetAllClientsQueryVariables = Exact<{ [key: string]: never; }>;
 
 
-export type GetAllClientsQuery = { __typename?: 'Query', queryAllClient: Array<{ __typename?: 'Client', id?: number | null, name?: string | null, phone?: string | null } | null> };
+export type GetAllClientsQuery = { __typename?: 'Query', getAllClients?: { __typename?: 'Client', id?: number | null, name?: string | null, phone?: string | null } | null };
 
 
 export const GetAllClientsDocument = gql`
     query GetAllClients {
-  queryAllClient {
+  getAllClients(page: 1) {
     id
     name
     phone
