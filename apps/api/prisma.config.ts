5d6d78ee74c9021212965ad3367f4aac4ddf0a49
Erick Cesar <e.cesar88@gmail.com>
2025-08-30 00:55:06 -0300

    feat: upgraded everything to latest version. Now it's time for tailwind

--- apps/api/prisma.config.ts
+++ apps/api/prisma.config.ts
@@ -1,15 +1,16 @@
 import path from 'node:path'
-import { defineConfig } from 'prisma'
+import { defineConfig } from 'prisma/config'
+
+const rootDatabasePath = ['src', 'database']
+const MIGRATIONS_PATH = [...rootDatabasePath, 'prisma', 'migrations']
+const SCHEMA_PATH = [...rootDatabasePath, 'prisma', 'schema.prisma']
+
+import 'dotenv/config'
 
 export default defineConfig({
-  schema: path.join('prisma', 'schema.prisma'),
+  schema: path.join(...SCHEMA_PATH),
   migrations: {
-    path: path.join('db', 'migrations')
-  },
-  views: {
-    path: path.join('db', 'views')
-  },
-  typedSql: {
-    path: path.join('db', 'queries')
+    path: path.join(...MIGRATIONS_PATH),
+    seed: 'pnpm ts-node-dev src/seeds/index.ts'
   }
 })
